8a3719d128c15138a52e3edeb4bd6a6d06777df8
tuannd255 <tuannd255@gmail.com>
2017-02-08 10:20:08 +0700

    fix bug show user

--- app/models/trainee_course.rb
+++ app/models/trainee_course.rb
@@ -4,7 +4,7 @@ class TraineeCourse < UserCourse
 
   alias_attribute :trainee, :user
 
-  has_many :user_subjects, ->{with_deleted}, foreign_key: :user_course_id, dependent: :destroy
+  has_many :user_subjects, foreign_key: :user_course_id, dependent: :destroy
 
   delegate :name, to: :trainee, prefix: true, allow_nil: true
 end
